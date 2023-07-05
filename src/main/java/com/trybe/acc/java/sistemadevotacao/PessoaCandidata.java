21c536261fc9f376e11b3a33cffa3c059356f05c
lorena-mendes <lorena.lccma@gmail.com>
2023-07-05 20:33:18 -0300

    feat: classe pessoa implementada

--- src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,42 @@
 package com.trybe.acc.java.sistemadevotacao;
 
-public class PessoaCandidata {
+
+public class PessoaCandidata extends Pessoa {
+  private int numero;
+  private int votos;
+
+  PessoaCandidata(String nome, int numero) {
+    super(nome);
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+  public String getNome() {
+    return super.getNome();
+  }
+
+  public void setNome(String nome) {
+    super.setNome(nome);
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void setVotos(int votos) {
+    this.votos = votos;
+  }
+
+  public void receberVoto() {
+    this.votos++;
+  }
 
 }
